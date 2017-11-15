eef8bc0fb4413b1f2cc34a09c1446a9f23b19a52
=tobilobaezekiel <owo.ezekiel@gmail.com>
2017-11-16 00:24:40 +0100

    added README file

--- src/spelling/AutoCompleteDictionaryTrie.java
+++ src/spelling/AutoCompleteDictionaryTrie.java
@@ -6,85 +6,97 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-/** 
+/**
  * An trie data structure that implements the Dictionary and the AutoComplete ADT
- * @author You
+ * @author Owolabi Tobiloba
  *
  */
 public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
 
     private TrieNode root;
     private int size;
-    
+
 
     public AutoCompleteDictionaryTrie()
 	{
 		root = new TrieNode();
 	}
-	
-	
+
+
 	/** Insert a word into the trie.
-	 * For the basic part of the assignment (part 2), you should convert the 
-	 * string to all lower case before you insert it. 
-	 * 
-	 * This method adds a word by creating and linking the necessary trie nodes 
-	 * into the trie, as described outlined in the videos for this week. It 
-	 * should appropriately use existing nodes in the trie, only creating new 
-	 * nodes when necessary. E.g. If the word "no" is already in the trie, 
-	 * then adding the word "now" would add only one additional node 
+	 * For the basic part of the assignment (part 2), you should convert the
+	 * string to all lower case before you insert it.
+	 *
+	 * This method adds a word by creating and linking the necessary trie nodes
+	 * into the trie, as described outlined in the videos for this week. It
+	 * should appropriately use existing nodes in the trie, only creating new
+	 * nodes when necessary. E.g. If the word "no" is already in the trie,
+	 * then adding the word "now" would add only one additional node
 	 * (for the 'w').
-	 * 
+	 *
 	 * @return true if the word was successfully added or false if it already exists
 	 * in the dictionary.
 	 */
-	public boolean addWord(String word)
-	{
-	    //TODO: Implement this method.
-	    return false;
+
+    public boolean search(String word)
+    {
+        TrieNode current = root;
+        for (char ch : word.toCharArray() )
+        {
+            if (current.subNode(ch) == null)
+                return false;
+            else
+                current = current.subNode(ch);
+        }
+        if (current.isEnd == true)
+            return true;
+        return false;
+    }
+	public boolean addWord(String word){
+		word = word.toLowerCase();
+
 	}
-	
-	/** 
+
+	/**
 	 * Return the number of words in the dictionary.  This is NOT necessarily the same
 	 * as the number of TrieNodes in the trie.
 	 */
-	public int size()
-	{
-	    //TODO: Implement this method
-	    return 0;
+	public int size(){
+		return size;
 	}
-	
-	
+
+
 	/** Returns whether the string is a word in the trie, using the algorithm
 	 * described in the videos for this week. */
 	@Override
-	public boolean isWord(String s) 
+	public boolean isWord(String s)
 	{
 	    // TODO: Implement this method
 		return false;
 	}
 
-	/** 
+	/**
      * Return a list, in order of increasing (non-decreasing) word length,
-     * containing the numCompletions shortest legal completions 
-     * of the prefix string. All legal completions must be valid words in the 
-     * dictionary. If the prefix itself is a valid word, it is included 
-     * in the list of returned words. 
-     * 
-     * The list of completions must contain 
-     * all of the shortest completions, but when there are ties, it may break 
-     * them in any order. For example, if there the prefix string is "ste" and 
-     * only the words "step", "stem", "stew", "steer" and "steep" are in the 
-     * dictionary, when the user asks for 4 completions, the list must include 
-     * "step", "stem" and "stew", but may include either the word 
+     * containing the numCompletions shortest legal completions
+     * of the prefix string. All legal completions must be valid words in the
+     * dictionary. If the prefix itself is a valid word, it is included
+     * in the list of returned words.
+     *
+     * The list of completions must contain
+     * all of the shortest completions, but when there are ties, it may break
+     * them in any order. For example, if there the prefix string is "ste" and
+     * only the words "step", "stem", "stew", "steer" and "steep" are in the
+     * dictionary, when the user asks for 4 completions, the list must include
+     * "step", "stem" and "stew", but may include either the word
      * "steer" or "steep".
-     * 
+     *
      * If this string prefix is not in the trie, it returns an empty list.
-     * 
+     *
      * @param prefix The text to use at the word stem
      * @param numCompletions The maximum number of predictions desired.
      * @return A list containing the up to numCompletions best predictions
      */@Override
-     public List<String> predictCompletions(String prefix, int numCompletions) 
+     public List<String> predictCompletions(String prefix, int numCompletions)
      {
     	 // TODO: Implement this method
     	 // This method should implement the following algorithm:
@@ -100,7 +112,7 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
     	 //       If it is a word, add it to the completions list
     	 //       Add all of its child nodes to the back of the queue
     	 // Return the list of completions
-    	 
+
          return null;
      }
 
@@ -109,22 +121,22 @@ public class AutoCompleteDictionaryTrie implements  Dictionary, AutoComplete {
  	{
  		printNode(root);
  	}
- 	
+
  	/** Do a pre-order traversal from this node down */
  	public void printNode(TrieNode curr)
  	{
- 		if (curr == null) 
+ 		if (curr == null)
  			return;
- 		
+
  		System.out.println(curr.getText());
- 		
+
  		TrieNode next = null;
  		for (Character c : curr.getValidNextCharacters()) {
  			next = curr.getChild(c);
  			printNode(next);
  		}
  	}
- 	
 
-	
+
+
 }
